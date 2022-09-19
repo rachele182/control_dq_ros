2ba39c1b934d4b4c6c61f7710257a5d45e85ea36
rachele182 <75611841+rachele182@users.noreply.github.com>
2022-09-19 13:58:08 +0200

    added plots from rosbags

--- rosbag/extract_demo3_data.m
+++ rosbag/extract_demo3_data.m
@@ -2,9 +2,9 @@
 clear all; close all; clc;
 
 %% INPUTS
-%% trivial solution with thight grasp
-filename = 'demo_box_no_adm.bag';       % Rosbag to extract
-save_name = "no_admittance.mat";        % Name of dest mat file
+%% solution with fixed stiffness
+filename = 'demo_box_no_mod_k.bag';       % Rosbag to extract
+save_name = "fixed_impedance.mat";        % Name of dest mat file
 
 %% Extract data from rosbag
 
@@ -14,16 +14,19 @@ bag = rosbag(filename);
 
 disp('Opened bag file.');
 
-bag.AvailableTopics; 
+bag.AvailableTopics
 
 bSel = select(bag,'Topic','/motion_control_dq/info_debug'); 
 bSel_traj = select(bag,'Topic','/motion_control_dq/dq_trajectory'); 
+bSel_comp = select(bag,'Topic','/motion_control_dq/compliant_traj'); 
 
 msgStructs = readMessages(bSel,'DataFormat','struct'); %%read message from dual arm control node
 msgStructs_traj = readMessages(bSel_traj,'DataFormat','struct'); %%read message from dual arm  trajectory gen node
+msgStructs_comp = readMessages(bSel_comp,'DataFormat','struct'); %%read message from dual
 
 len_msg = length(msgStructs); 
 len_msg_traj = length(msgStructs_traj); 
+len_msg_comp = length(msgStructs_comp); 
 
 %% Variables from trajectory gen node
 stamp_traj= zeros(1,len_msg_traj);
@@ -41,6 +44,27 @@ for i = 1 : length(msgStructs_traj)
 end
 
 disp('Read Relative position.');
+
+%% Variables from admittance loop
+stamp_comp = zeros(1,len_msg_comp);
+pos_comp_rel = zeros(3,len_msg_comp); %compliant relative position
+frel = zeros(3,len_msg_comp); %relative force 
+stiffness = zeros(3,len_msg_comp);  %relative stiffnes 
+t_bias_2 = double(msgStructs_comp{1}.Header.Stamp.Sec)+...
+double(msgStructs_comp{1}.Header.Stamp.Nsec)*10^-9;
+
+for i = 1 : length(msgStructs_comp)
+    ros_t = msgStructs_comp{i}.Header.Stamp;
+    stamp_comp(i) = double(ros_t.Sec)+double(ros_t.Nsec)*10^-9;
+    stamp_comp(i) = stamp_comp(i)-t_bias_2;
+    pos_comp_rel(:,i) = msgStructs_comp{i}.PosRelC(1:3);
+    frel(:,i) = msgStructs_comp{i}.FrF(1:3);
+    stiffness(:,i) = msgStructs_comp{i}.Stiff(1:3);
+  
+end
+
+disp('Read Compliant variables.');
+
 
 %% Variables from control node
 fl = zeros(3,len_msg); %left arm
@@ -62,11 +86,34 @@ end
 disp('Read Forces.');
 
 %% Post processing
-
 % Getting minimum resolution time
-max_t = stamp_traj(end);
-dt = mean(diff(stamp_traj));
+max_t = max([stamp_traj(end), stamp_comp(end)]); 
+dt = max([mean(diff(stamp_traj)),mean(diff(stamp_comp))]); 
 time  =  0 : dt : max_t;
+
+
+pos_nom_pp  =  zeros(size(pos_nom,1),size(time,2));
+pos_comp_rel_pp  =  zeros(size(pos_comp_rel,1),size(time,2));
+f_rel_pp = zeros(size(frel,1),size(time,2)); 
+stiff_pp = zeros(size(stiffness,1),size(time,2)); 
+
+for i  =  1 : size(time,2)
+    [~, ix]  =  min(abs(stamp_traj-time(i)));
+    pos_nom_pp(:,i)  =  pos_nom(:, ix);
+    
+    [~, ix]  =  min(abs(stamp_comp-time(i)));
+    pos_comp_rel_pp(:,i)  =  pos_comp_rel(:, ix);
+    
+    [~, ix]  =  min(abs(stamp_comp-time(i)));
+    f_rel_pp(:,i)  =  frel(:, ix);
+    
+    [~, ix]  =  min(abs(stamp_comp-time(i)));
+    stiff_pp(:,i) = stiffness(:,ix); 
+end
+
+disp('Scaled time.');
+
+
 
 %% Save the extracted data
 
@@ -75,12 +122,11 @@ save_path = fullfile(pwd,save_name);
 if isfile(save_path)
     msg = 'WARNING: The specified file already exists.';
     disp(msg);
-%else
-    save(save_path,'fl','fr','time','pos_nom','pos_r','pos_1','pos_2');
 end
+    save(save_path,'fl','fr','stiff_pp','f_rel_pp','pos_comp_rel_pp','time','pos_nom_pp','pos_r','pos_1','pos_2');
 
 %% Plotting demo with trivial solution
-load("no_admittance.mat");
+load("fixed_impedance.mat");
 
 %%Define color
 red = [0.8 0.2 0.2]; 
@@ -89,16 +135,16 @@ or = [0.9 0.5 0.1];
 
 
 %% forces
-tt = 0:0.001:15; 
-sizet = size(tt,2); 
+tt = 0:0.001:17.3; 
+sizet = size(tt,2);
+lim2 = 17.3; 
 f = figure;
 f.Renderer = 'painters';
 subplot(2, 1, 1)
 grid on
 hold on
 plot(tt,fl(2,1:sizet),'LineWidth',1.5,'Color',red);
-xlabel('$t/\mathrm{s}$', 'Interpreter', 'latex', 'FontSize', 12)
-% xlim([1.8,4])
+xlim([0,lim2])
 ylabel('$f_1/\mathrm{N}$', 'Interpreter', 'latex', 'FontSize', 12)
 
 subplot(2, 1, 2)
@@ -106,12 +152,10 @@ grid on
 hold on
 plot(tt,fr(2,1:sizet),'LineWidth',1.5);
 xlabel('$t/\mathrm{s}$', 'Interpreter', 'latex', 'FontSize', 12)
-% xlim([1.8,4])
+xlim([0,lim2])
 ylabel('$f_2/\mathrm{N}$', 'Interpreter', 'latex', 'FontSize', 12)
 
 %% arms position y-axis
-tt = 0:0.001:15; 
-sizet = size(tt,2); 
 f = figure;
 f.Renderer = 'painters';
 subplot(2, 1, 1)
@@ -119,8 +163,8 @@ grid on
 hold on
 plot(tt,pos_1(2,1:sizet),'LineWidth',1.5,'Color',or);
 grid on; hold on
+xlim([0,lim2])
 %%plot(tt,0.92*ones(sizet,1),'r--','LineWidth',1.5); 
-% xlim([1.8,4])
 ylabel('$y_1/\mathrm{m}$', 'Interpreter', 'latex', 'FontSize', 12)
 
 subplot(2, 1, 2)
@@ -128,8 +172,8 @@ grid on
 hold on
 plot(tt,pos_2(2,1:sizet),'LineWidth',1.5,'Color',gr);
 xlabel('$t/\mathrm{s}$', 'Interpreter', 'latex', 'FontSize', 12)
-% xlim([1.8,4])
 grid on; hold on
+xlim([0,lim2])
 %%plot(tt,0.63*ones(sizet,1),'r--','LineWidth',1.5); 
 ylabel('$y_2/\mathrm{m}$', 'Interpreter', 'latex', 'FontSize', 12)
 
@@ -138,18 +182,40 @@ f = figure;
 f.Renderer = 'painters';
 grid on
 hold on
-plot(time(1,:),pos_nom(3,:),'LineWidth',1.5);
+plot(time(1,:),pos_nom_pp(3,:),'LineWidth',1.5);
 grid on
 hold on
-plot(tt,pos_r(3,1:sizet),'LineWidth',1.5,'Color',[0.5 0 0.5]);
+plot(time(1,:),pos_comp_rel_pp(3,:),'LineWidth',1.5);
 hold on 
 grid on
-plot(tt,0.26*ones(sizet,1),'LineStyle','--','LineWidth',1); 
+plot(tt,0.26*ones(sizet,1),'LineStyle','--','LineWidth',1.5); 
+hold on 
+grid on
+plot(tt,pos_r(3,1:sizet),'LineWidth',1.5,'Color',[0.5 0 0.5]);
 xlabel('$t/\mathrm{s}$', 'Interpreter', 'latex', 'FontSize', 12)
-xlim([0 15])
+xlim([0 lim2])
 ylabel('$z/\mathrm{m}$', 'Interpreter', 'latex', 'FontSize', 12)
-legend('ref','curr','contact point','Interpreter', 'latex', 'FontSize', 10)
+legend('ref','comp','contact point','curr','Interpreter', 'latex', 'FontSize', 10)
 
+%% Relative translational stiffness
+% f = figure;
+% f.Renderer = 'painters';
+% grid on
+% hold on
+% plot(time(1,:),stiff_pp(3,:),'LineWidth',1.5,'Color',[0.5 0 0.5]);
+% xlabel('$t/\mathrm{s}$', 'Interpreter', 'latex', 'FontSize', 12)
+% xlim([0 20])
+% ylabel('$K/\mathrm{Nm}$', 'Interpreter', 'latex', 'FontSize', 12)
+
+%% relative force z-axis
+f = figure;
+f.Renderer = 'painters';
+grid on
+hold on
+plot(time(1,:),f_rel_pp(3,:),'LineWidth',1.5);
+xlabel('$t/\mathrm{s}$', 'Interpreter', 'latex', 'FontSize', 12)
+xlim([0 lim2])
+ylabel('$f_r/\mathrm{N}$', 'Interpreter', 'latex', 'FontSize', 12)
 
 %% Analysis
 f = figure;
@@ -158,37 +224,31 @@ subplot(2,2,1);
 grid on
 hold on
 plot(tt,fl(2,1:sizet),'LineWidth',1.5,'Color',red);
+xlim([0,lim2])
 ylabel('$f/\mathrm{N}$', 'Interpreter', 'latex', 'FontSize', 12)
-legend('left arm','Interpreter', 'latex', 'FontSize', 10)
+
 
 subplot(2,2,2);
 grid on
 hold on
 plot(tt,fr(2,1:sizet),'LineWidth',1.5);
+xlim([0,lim2])
 % ylabel('$f_2/\mathrm{N}$', 'Interpreter', 'latex', 'FontSize', 12)
-legend('right arm','Interpreter', 'latex', 'FontSize', 10)
-
 
 subplot(2,2,3);
 grid on
 hold on
 plot(tt,pos_1(2,1:sizet),'LineWidth',1.5,'Color',or);
 xlabel('$t/\mathrm{s}$', 'Interpreter', 'latex', 'FontSize', 12)
+xlim([0,lim2])
 % xlim([1.8,4])
 ylabel('$y/\mathrm{m}$', 'Interpreter', 'latex', 'FontSize', 12)
-legend('left pos','Interpreter', 'latex', 'FontSize', 10)
 
 subplot(2,2,4);
 grid on
 hold on
 plot(tt,pos_2(2,1:sizet),'LineWidth',1.5,'Color',gr);
+xlim([0,lim2])
 xlabel('$t/\mathrm{s}$', 'Interpreter', 'latex', 'FontSize', 12)
-legend('right pos','Interpreter', 'latex', 'FontSize', 10)
-
 % xlim([1.8,4])
 % ylabel('$y_2/\mathrm{m}$', 'Interpreter', 'latex', 'FontSize', 12)
-
-
-
-
-
